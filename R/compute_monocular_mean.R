583103be3aca41628273973121d7fbbcf4f2b25c
jgeller <gellerja@bc.edu>
2025-11-06 11:16:46 -0500

    ADD MONOCULAR MEAN.RD

--- R/compute_monocular_mean.R
+++ R/compute_monocular_mean.R
@@ -1,10 +1,16 @@
-# A "monocular mean" averages both eyes together. If data is available in just
-# one eye, use the available value as the mean, unless we need_both is TRUE.
-#' @param x1 pupil left
-#' @param x2 pupil right
-#' @return vector with monocular mean values
+#' Compute a Monocular Mean Pupil Value
+#'
+#' A "monocular mean" averages both eyes together. If data is available in just
+#' one eye, use the available value as the mean. If both eyes are missing, the
+#' result is `NA`.
+#'
+#' @param x1 Numeric vector for the left pupil.
+#' @param x2 Numeric vector for the right pupil.
+#'
+#' @return A numeric vector of monocular mean pupil values.
+#' @export
 compute_monocular_mean <- function(x1, x2) {
   xm <- rowMeans(cbind(x1, x2), na.rm = TRUE)
-  # NaN => NA
+  # Replace NaN (caused when both are NA) with NA
   ifelse(is.nan(xm), NA, xm)
 }
